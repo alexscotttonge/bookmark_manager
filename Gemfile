fc925388897189e3a0b3230f8361f0c3ad6a0c78
Alex <alex@scotttonge.com>
2017-10-18 17:01:17 +0100

    Connected postgresql hooby-dev with Heroku

--- Gemfile
+++ Gemfile
@@ -6,12 +6,12 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
-gem 'capybara'
-gem 'rspec'
 gem 'sinatra'
+gem 'database_cleaner'
 
 group :test do
-  gem 'database_cleaner'
+  gem 'capybara'
+  gem 'rspec'
 end
 # dm-migrations
 # gem "rails"
--- app/models/link.rb
+++ app/models/link.rb
@@ -16,7 +16,7 @@ end
 # display logs
 DataMapper::Logger.new($stdout, :debug)
 # set up a connection with a database
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 # check everything written in the models is OK
 DataMapper.finalize
 # build any new columns or tables added. Also doesn't overwrite any existing data.
